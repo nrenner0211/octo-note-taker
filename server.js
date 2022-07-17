f4b35c2cd5c3ed641cfbb494c86148acf2c7a778
nrenner <nrenner0211@gmail.com>
2022-07-16 20:05:33 -0400

    server connected

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Express middleware
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname));
 
-
+require('./routes/routes')(app);
 
 // Default response for any other request (Not Found) //catchall
 app.use((req, res) => {
